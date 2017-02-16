f6248cc16c473ca98fe02bc9dba314e5a245c7b1
Jonathan Janssen <jjanseen@gmail.com>
2017-02-16 14:02:50 -0700

    Added initial features to set source folders and destination folder and run the un duplicate process

---
+++ FileUnduplicator/ViewModels/Folder.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileUnduplicator.ViewModels
+{
+    public class Folder : BaseViewModel
+    {
+        public int ID { get; set; }
+        public string FolderPath { get; set; }
+    }
+}
---
+++ FileUnduplicator/ViewModels/FolderCollectionViewModel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileUnduplicator.ViewModels
+{
+    public class FolderCollectionViewModel : BaseViewModel
+    {
+        public FolderCollectionViewModel()
+        {
+            Folders = new ObservableCollection<Folder>();
+        }
+
+        private string _destinationFolderPath;
+        public string DestinationFolderPath
+        {
+            get
+            {
+                return _destinationFolderPath;
+            }
+            set
+            {
+                if (_destinationFolderPath != value)
+                {
+                    _destinationFolderPath = value;
+                    OnPropertyChanged("DestinationFolderPath");
+                }
+            }
+        }
+
+        private ObservableCollection<Folder> _folders;
+        public ObservableCollection<Folder> Folders
+        {
+            get
+            {
+                return _folders;
+            }
+            set
+            {
+                if (_folders != value)
+                {
+                    _folders = value;
+                    OnPropertyChanged("Folders");
+                }
+            }
+        }
+
+        public int GetNextUploadFolderID()
+        {
+            if (Folders.Count == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return Folders.Max(f => f.ID) + 1;
+            }
+        }
+    }
+}
